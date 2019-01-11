ec2f33131bcfc419d9608d802a8222750f50d8e2
Gergely Imreh <imrehg@gmail.com>
2019-01-11 19:51:01 +0000

    test
    Signed-off-by: Gergely Imreh <imrehg@gmail.com>

---
+++ start.sh
@@ -0,0 +1,52 @@
+
+
+function mount_dev()
+{
+	mkdir -p /tmp
+	mount -t devtmpfs none /tmp
+	mkdir -p /tmp/shm
+	mount --move /dev/shm /tmp/shm
+	mkdir -p /tmp/mqueue
+	mount --move /dev/mqueue /tmp/mqueue
+	mkdir -p /tmp/pts
+	mount --move /dev/pts /tmp/pts
+	touch /tmp/console
+	mount --move /dev/console /tmp/console
+	umount /dev || true
+	mount --move /tmp /dev
+
+	# Since the devpts is mounted with -o newinstance by Docker, we need to make
+	# /dev/ptmx point to its ptmx.
+	# ref: https://www.kernel.org/doc/Documentation/filesystems/devpts.txt
+	ln -sf /dev/pts/ptmx /dev/ptmx
+	mount -t debugfs nodev /sys/kernel/debug
+}
+
+function start_udev()
+{
+	if [ "$UDEV" == "on" ]; then
+		if $PRIVILEGED; then
+			mount_dev
+			if command -v udevd &>/dev/null; then
+				unshare --net udevd --daemon &> /dev/null
+			else
+				unshare --net /lib/systemd/systemd-udevd --daemon &> /dev/null
+			fi
+			udevadm trigger &> /dev/null
+		else
+			echo "Unable to start udev, container must be run in privileged mode to start udev!"
+		fi
+	fi
+}
+
+UDEV=on
+start_udev
+
+systemctl start mnt-storage.mount
+
+systemctl status mnt-storage.mount
+
+while : ; do
+ echo "Idling..."
+ sleep 600
+done
